86d6b29d1af5a7b8c237d3fa3b6f01e69fe3930b
Mariya <mariya.podosinova@gmail.com>
2020-09-30 14:32:10 +0100

    starter code

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
-      {notes.map((note) => (
-        <Note key={note.id} title={note.title} content={note.content} />
-      ))}
-
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
